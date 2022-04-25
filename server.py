6737375080180b7736b0b8e9dfb53df2a80a9592
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-04-25 20:46:53 -0300

    Change virtual env library

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from dexpression import Dexpression
-from fastai.vision.widgets import * 
+# from dexpression import Dexpression
+# from fastai.vision.widgets import * 
 
 app = Flask(__name__)
 
