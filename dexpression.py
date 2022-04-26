7c6ec30a923028fae432a7ad42c051e3870d073a
Paulo Filho <paulomacieltorresfilho@gmail.com>
2022-04-26 12:58:11 -0300

    modelo adicionado e exemplo com imagem feito

--- dexpression.py
+++ dexpression.py
@@ -1,3 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
 class Dexpression(nn.Module):
     def __init__(self, data_Channels,data_Dim, data_Features):
 
--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 from flask import Flask
-# from dexpression import Dexpression
-# from fastai.vision.widgets import * 
+from dexpression import Dexpression
+from fastai.vision.all import load_learner
+import pathlib
 
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
+
+learner = load_learner('64pctmodel.pkl')
+result = learner.predict('./teste.jpg')
 app = Flask(__name__)
 
 # Deafult API route
 @app.route("/")
 def homepage():
-    return "Hello World"
+    return result[0]
 
 if __name__ == "__main__":
     app.run(host='localhost', port=9874)
